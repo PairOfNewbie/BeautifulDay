789c001b009de030bf46eb00580f979b1fd9509a
DaiFengyi <daifengji@163.com>
2016-04-04 21:42:26 +0800

    Project and Pod init

---
+++ BeautifulDay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BeautifulDay
+//
+//  Created by DaiFengyi on 16/4/4.
+//  Copyright © 2016年 PairOfNewbie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,17 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '8.0'
+# Uncomment this line if you're using Swift
+# use_frameworks!
+
+target 'BeautifulDay' do
+
+end
+
+target 'BeautifulDayTests' do
+
+end
+
+target 'BeautifulDayUITests' do
+
+end
+
