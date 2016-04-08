6c04b5116fed6f156a41c14e670a268ff80ebaff
DaiFengyi <daifengji@163.com>
2016-04-08 16:15:07 +0800

    Track with bridging header

---
+++ BeautifulDay/Module/Track.h
@@ -0,0 +1,13 @@
+//
+//  Track.h
+//  BeautifulDay
+//
+//  Created by DaiFengyi on 16/4/8.
+//  Copyright © 2016年 PairOfNewbie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Track : NSObject
+
+@end
---
+++ BeautifulDay/Module/Track.m
@@ -0,0 +1,13 @@
+//
+//  Track.m
+//  BeautifulDay
+//
+//  Created by DaiFengyi on 16/4/8.
+//  Copyright © 2016年 PairOfNewbie. All rights reserved.
+//
+
+#import "Track.h"
+
+@implementation Track
+
+@end
