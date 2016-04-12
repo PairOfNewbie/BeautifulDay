cd591d0d714067beafa4216a1d48c149b9b94114
DaiFengyi <daifengji@163.com>
2016-04-12 12:02:01 +0800

    Update source

--- BeautifulDay/Module/BDMusicDayController.swift
+++ BeautifulDay/Module/BDMusicDayController.swift
@@ -9,8 +9,10 @@
 import UIKit
 import DOUAudioStreamer
 let bottomHeight : CGFloat = 44 + 2
+
 class BDMusicDayController: UIViewController, UIScrollViewDelegate {
     var pageIndex : Int = 0
+    var trk :Track? = nil
     
     @IBOutlet weak var bgView: UIImageView!
     @IBOutlet weak var dateLabel: UILabel!
@@ -28,6 +30,14 @@ class BDMusicDayController: UIViewController, UIScrollViewDelegate {
     }
     
     //MARK: - Life Cycle
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        trk = Track()
+        trk?.artist = "jason"
+        trk?.title = "jason's song"
+//        trk?.audioFileURL = NSURL(string: "http://passagetellsproject.net/app/brixton/0000.mp3")
+//        resetStreamer()
+    }
     //MARK: - Initial Setup
     func setupSubviews() {
         let panGesture = UIPanGestureRecognizer(target: self, action: NSSelectorFromString("onPan:"))
@@ -35,11 +45,34 @@ class BDMusicDayController: UIViewController, UIScrollViewDelegate {
         let tapGesture = UITapGestureRecognizer(target: self, action: NSSelectorFromString("onTap:"))
         mpView.addGestureRecognizer(tapGesture)
     }
-
-    //MARK: - DOUPlayer
-    var streamer = DOUAudioStreamer()
-//    var audioVisualizer = DOUAudioVisualizer(frame: CGRectZero)
     
+    //MARK: - DOUPlayer
+    var streamer : DOUAudioStreamer? = DOUAudioStreamer()
+    //    var audioVisualizer = DOUAudioVisualizer(frame: CGRectZero)
+    func canncelStreamer() {
+        if let s = streamer {
+//            s.removeObserver(self, forKeyPath: "status")
+//            s.removeObserver(self, forKeyPath: "duration")
+//            s.removeObserver(self, forKeyPath: "bufferingRatio")
+            streamer = nil
+        }
+    }
+    
+    func resetStreamer() {
+        canncelStreamer()
+        assert(trk?.audioFileURL != nil, "the audio url is nil")
+        streamer = DOUAudioStreamer(audioFile: trk)
+//        streamer?.addObserver(self, forKeyPath: "status", options: .New, context: nil)
+//        streamer?.addObserver(self, forKeyPath: "duration", options: .New, context: nil)
+//        streamer?.addObserver(self, forKeyPath: "bufferingRatio", options: .New, context: nil)
+        streamer?.play()
+        
+    }
+    
+    //MARK: - KVO
+    override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+        
+    }
     
     //MARK: - Gesture
     var origin : CGPoint = CGPointZero
@@ -73,7 +106,7 @@ class BDMusicDayController: UIViewController, UIScrollViewDelegate {
             }
             var f = self.view.bounds
             f.origin = finalOrigin
-            UIView.animateWithDuration(0.3, animations: { 
+            UIView.animateWithDuration(0.3, animations: {
                 self.mpView.transform = CGAffineTransformIdentity
                 self.mpView.frame = f
                 }, completion: { (finished: Bool) in
