0609564f97197dfb92dca972f0f30a53517ad8f1
DaiFengyi <daifengji@163.com>
2016-05-07 17:32:31 +0800

    Model init

---
+++ BeautifulDay/Model/Model.swift
@@ -0,0 +1,11 @@
+//
+//  Model.swift
+//  BeautifulDay
+//
+//  Created by DaiFengyi on 16/5/7.
+//  Copyright © 2016年 PairOfNewbie. All rights reserved.
+//
+
+// todo: how to use : https://github.com/tristanhimmelman/AlamofireObjectMapper
+
+import Foundation
