76366e62ad9f0bb85f863c0d48d855073889cf08
DaiFengyi <daifengji@163.com>
2016-06-16 10:50:55 +0800

    Login and Register

---
+++ BeautifulDay/Controller/Login/SAILoginMainController.m
@@ -0,0 +1,95 @@
+//
+//  SAILoginMainController.m
+//  sai-iOS
+//
+//  Created by DaiFengyi on 15/11/5.
+//  Copyright © 2015年 Malong Tech. All rights reserved.
+//
+
+#import "SAILoginMainController.h"
+//#import "SAIUtil.h"
+#import "SAIRegisterController.h"
+#import "ShareSDK/ShareSDK.h"
+@interface SAILoginMainController ()
+@property (weak, nonatomic) IBOutlet UITextField *phoneTextField;
+
+@end
+
+@implementation SAILoginMainController
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    self.title = @"注册";
+    UIBarButtonItem *left = [[UIBarButtonItem alloc] initWithTitle:@"取消" style:UIBarButtonItemStylePlain target:self action:@selector(dismissSelf)];
+    self.navigationItem.leftBarButtonItem = left;
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    [self.view endEditing:YES];
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    [self.view endEditing:YES];
+}
+
+#pragma mark - btn delegate
+- (void)dismissSelf {
+    if (self.navigationController != nil) {
+        [self.navigationController dismissViewControllerAnimated:YES
+                                                      completion:^{
+                                                          [self.delegate cancel];
+                                                      }];
+    }
+}
+- (IBAction)WeiboLogin:(id)sender {
+    [self.delegate WeiboLogin];
+}
+
+- (IBAction)QQLogin:(id)sender {
+    [self.delegate QQLogin];
+}
+
+- (IBAction)WeichatLogin:(id)sender {
+    [self.delegate WeichatLogin];
+}
+- (IBAction)registerButtonClick:(UIButton *)sender {
+    if (![SAIUtil validateMobile:self.phoneTextField.text]) {
+        [SAIUtil showMsg:@"手机格式不正确"];
+        return;
+    }
+    [SAIUtil showLoading];
+    [SAIAuthentication sendValidateCodeByMobile:self.phoneTextField.text
+        success:^{
+            SAIRegisterController *rc = [[SAIRegisterController alloc] initWithPhone:self.phoneTextField.text];
+            [self.navigationController pushViewController:rc animated:YES];
+            [SAIUtil showMsg:@"验证码已发送"];
+        }
+        failure:^(NSError *error) {
+            if (error.code == 10008) {
+                [self loginButtonClick:nil];
+            } else {
+                [SAIMonitor trackError:monitor_eventLabel_error_sendRegistValidateCode error:error];
+            }
+            [SAIUtil showMsg:error.localizedDescription];
+        }];
+}
+
+- (IBAction)loginButtonClick:(UIButton *)sender {
+    SAILoginController *loginController = [[SAILoginController alloc] initWithAccount:self.phoneTextField.text];
+    [self.navigationController pushViewController:loginController animated:YES];
+    // loginController.accountTextField.text = self.phoneTextField.text;
+    //    [self.delegate EmailLogin:self.phoneTextField.text password:self.passwordTextField.text];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ BeautifulDay/Controller/Login/SAIRegisterController.h
@@ -0,0 +1,13 @@
+//
+//  SAIRegisterController.h
+//  sai-iOS
+//
+//  Created by DaiFengyi on 15/11/5.
+//  Copyright © 2015年 Malong Tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SAIRegisterController : UIViewController
+- (instancetype)initWithPhone:(NSString *)phone;
+@end
